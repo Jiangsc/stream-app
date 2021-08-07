c5243ea59aa1c2dac5bfe2de8e84261f1f94eb89
Jiangsc <sjiang24@hawk.iit.edu>
2021-08-07 18:41:51 -0400

    a bounch of features

--- streamapp/src/components/App.js
+++ streamapp/src/components/App.js
@@ -1,27 +1,28 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
+import history from "../history";
 
 //588883368652-ld7bcjt1ej1s6prh6ks96lvrlnch0mtt.apps.googleusercontent.com
 
 const App = () => {
     return (
         <div>
-            <BrowserRouter>
+            <Router history={history}>
                 <Header className="ui container" />
                 <div>
                     <Route exact path="/" component={StreamList} />
                     <Route path="/streams/new" component={StreamCreate} />
-                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/edit/:id" component={StreamEdit} />
                     <Route path="/streams/delete" component={StreamDelete} />
                     <Route path="/streams/show" component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 };
